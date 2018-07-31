cb22d4671b14ff65032a56938dd1aac0f1262ce4
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-07-31 18:00:11 +0100

    player can select best attribute (any of three)

--- models/specs/player_spec.js
+++ models/specs/player_spec.js
@@ -11,6 +11,11 @@ describe("Player", function () {
   beforeEach(function () {
     card1 = new Card("Superman", 6, 9, 7);
     card2 = new Card("Scarlet Witch", 7, 10, 5);
+    card3 = new Card("Black Widdow", 8, 6, 9);
+    card4 = new Card("The Flash", 7, 4, 10);
+    card5 = new Card("Wonder Woman", 8, 7, 5);
+    card6 = new Card("Batman", 10, 5, 6);
+
     player = new Player("Gary");
   });
 
@@ -36,11 +41,24 @@ describe("Player", function () {
       assert.deepStrictEqual(player.cards, [card2]);
     });
 
-    it("should be able to select attribute", function () {
+    it("should be able to select attribute intelligence", function () {
+      player.addCard(card6);
+      const result = player.selectAttribute();
+      assert.deepStrictEqual(result, "intelligence");
+    });
+
+    it("should be able to select attribute strength", function () {
       player.addCard(card1);
       const result = player.selectAttribute();
       assert.deepStrictEqual(result, "strength");
     });
+
+    it("should be able to select attribute agility", function () {
+      player.addCard(card4);
+      const result = player.selectAttribute();
+      assert.deepStrictEqual(result, "agility");
+    });
+
 
   });
 
